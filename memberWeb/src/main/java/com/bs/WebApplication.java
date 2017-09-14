147275f481c10fa5eeaaef7c2324cadfa50b3cfa
liuxl <liuxl@317hu.com>
2017-09-14 10:23:57 +0800

    修复端口信息 增加路由器模块

--- memberWeb/src/main/java/com/bs/WebApplication.java
+++ memberWeb/src/main/java/com/bs/WebApplication.java
@@ -4,14 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient//向服务中心注册
 @EnableFeignClients
 @EnableHystrixDashboard//开启hystrixDashboard,可以监控断路器
 @EnableCircuitBreaker//feign下开启监控断路器必须加
+@EnableZuulProxy //开启路由
 public class WebApplication {
 
 	public static void main(String[] args) {
